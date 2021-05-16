f115ceb6c1876ccdce264af8bc633628d84384ba
Sidharth Sundaram <sidharth.rama@gmail.com>
2021-05-16 11:54:13 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+if (window.performance.navigation.type === 1) {
+    rollDice();
+  }
 
-var randomImage = "dice" + randomNumber1 + ".png";
+function rollDice(){
+    var randomNumber1 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource = "images/" + randomImage;
+    var randomImage = "dice" + randomNumber1 + ".png";
 
-var image1 = document.querySelectorAll("img")[0];
+    var randomImageSource = "images/" + randomImage;
 
-image1.setAttribute("src", randomImageSource);
+    var image1 = document.querySelectorAll("img")[0];
 
-var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+    image1.setAttribute("src", randomImageSource);
 
-var randomImage2 = "dice" + randomNumber2 + ".png";
+    var randomNumber2 = Math.floor(Math.random() * 6) + 1;
 
-var randomImageSource2 = "images/" + randomImage2;
+    var randomImage2 = "dice" + randomNumber2 + ".png";
 
-var image2 = document.querySelectorAll("img")[1];
+    var randomImageSource2 = "images/" + randomImage2;
 
-image2.setAttribute("src", randomImageSource2);
+    var image2 = document.querySelectorAll("img")[1];
 
-if(randomNumber1 > randomNumber2){
-    document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    image2.setAttribute("src", randomImageSource2);
+
+    if(randomNumber1 > randomNumber2){
+        document.querySelector("h1").innerHTML = "Player 1 Wins!";
+    }
+    else if(randomNumber1 < randomNumber2) {
+        document.querySelector("h1").innerHTML = "Player 2 Wins!";
+    }
+    else 
+        document.querySelector("h1").innerHTML = "It's a draw.";
 }
-else if(randomNumber1 < randomNumber2) {
-    document.querySelector("h1").innerHTML = "Player 2 Wins!";
-}
-else 
-    document.querySelector("h1").innerHTML = "It's a draw.";
